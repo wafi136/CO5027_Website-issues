1778c0eb6bd0c6ad639ff7bba4cd9c9f94b96b65
wafi learn <wafi_it@outlook.com>
2018-05-04 11:15:26 +0800

    Final

--- Website-Ecom/product.aspx.cs
+++ Website-Ecom/product.aspx.cs
@@ -17,58 +17,7 @@ namespace Website_Ecom
 
         protected void btnBuy_Click(object sender, EventArgs e)
         {
-            decimal postagePackagingCost = 3.95m;
-            decimal productPrice = 10.00m;
-            int quantityofProduct = int.Parse(DDQty.SelectedValue);
-            decimal subTotal = (quantityofProduct * productPrice);
-            decimal total = subTotal + postagePackagingCost;
-
-            var config = ConfigManager.Instance.GetProperties();
-            var accessToken = new OAuthTokenCredential(config).GetAccessToken();
-            var apiContext = new APIContext(accessToken);
-
-            var productItem = new Item();
-            productItem.name = "Product 1";
-            productItem.currency = "BND";
-            productItem.price = productPrice.ToString();
-            productItem.sku = "PRO1";
-            productItem.quantity = quantityofProduct.ToString();
-
-            var transactionDetails = new Details();
-            transactionDetails.tax = "0";
-            transactionDetails.shipping = postagePackagingCost.ToString();
-            transactionDetails.subtotal = subTotal.ToString("0.00");
-
-            var transactionAmount = new Amount();
-            transactionAmount.currency = "BND";
-            transactionAmount.total = total.ToString("0.00");
-            transactionAmount.details = transactionDetails;
-
-            var transaction = new Transaction();
-            transaction.description = "Product 1 Description";
-            transaction.invoice_number = Guid.NewGuid().ToString();
-            transaction.amount = transactionAmount;
-            transaction.item_list = new ItemList
-            {
-                items = new List<Item> { productItem }
-            };
-
-            var payer = new Payer();
-            payer.payment_method = "paypal";
-
-            var redirectUrls = new RedirectUrls();
-            redirectUrls.cancel_url = "http://" + HttpContext.Current.Request.Url.Authority + "/cancel.aspx";
-            redirectUrls.return_url = "http://" + HttpContext.Current.Request.Url.Authority + "/completepurchase.aspx";
-
-            var payment = Payment.Create(apiContext, new Payment
-            {
-                intent = "sale",
-                payer = payer,
-                transactions = new List<Transaction> { transaction },
-                redirect_urls = redirectUrls
-            });
-
-            Session["paymentId"] = payment.id;
+           
         }
     }
 }
